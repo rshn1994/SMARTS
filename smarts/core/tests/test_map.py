d244f426e306fe7fa54ee87e972fd0366c83dd57
Rutvik Gupta <RutvikGupta@users.noreply.github.com>
2021-11-04 17:11:31 -0400

    test maps

--- smarts/core/tests/test_map.py
+++ smarts/core/tests/test_map.py
@@ -528,8 +528,8 @@ def test_od_map_lane_offset():
     # # point not on lane but on road
     point = (31.0, 4.5, 0)
     refline_pt = l0.to_lane_coord(point)
-    assert round(refline_pt.s, 2) == 6.0
-    assert round(refline_pt.t, 2) == 4.5
+    assert round(refline_pt.s, 2) == 6.19
+    assert round(refline_pt.t, 2) == 4.19
 
     offset = refline_pt.s
     assert l0.width_at_offset(offset) == 3.25
