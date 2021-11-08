d4f9f8c5053d9775cada8b96f0e96eee8b3f341b
Rutvik Gupta <RutvikGupta@users.noreply.github.com>
2021-11-08 17:36:59 -0500

    fix for vector at offset

--- smarts/core/tests/test_map.py
+++ smarts/core/tests/test_map.py
@@ -252,8 +252,8 @@ def test_od_map_junction():
     # point not on lane but on road
     point = (122.0, 170.0, 0)
     refline_pt = l1.to_lane_coord(point)
-    assert round(refline_pt.s, 2) == 33.0
-    assert round(refline_pt.t, 2) == -2.0
+    assert round(refline_pt.s, 2) == 70.0
+    assert round(refline_pt.t, 2) == 2.0
 
     offset = refline_pt.s
     assert l1.width_at_offset(offset) == 3.75
