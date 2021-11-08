b7e8519b679e240800c756e1021b76a4507a5e21
Rutvik Gupta <RutvikGupta@users.noreply.github.com>
2021-11-08 17:56:23 -0500

    bug fix

--- smarts/core/opendrive_road_network.py
+++ smarts/core/opendrive_road_network.py
@@ -898,7 +898,10 @@ class OpenDriveRoadNetwork(RoadMap):
             return super().curvature_radius_at_offset(offset, lookahead)
 
         def width_at_offset(self, lane_point_s: float) -> float:
-            road_offset = lane_point_s + self.road.s_pos
+            if self.index < 0:
+                road_offset = lane_point_s + self.road.s_pos
+            else:
+                road_offset = (self._length - lane_point_s) + self.road.s_pos
             inner_boundary, outer_boundary = self._lane_boundaries
             t_outer = outer_boundary.calc_t(road_offset, self.road.s_pos, self.index)
             t_inner = inner_boundary.calc_t(road_offset, self.road.s_pos, self.index)
